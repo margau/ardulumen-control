9193c8367e22b01cf6100d6e5de662297f82c1b5
Marvin Gaube <dev@marvingaube.de>
2019-11-18 13:11:52 +0100

    Preparations with display and Menu structure

--- ardulumen-control.ino
+++ ardulumen-control.ino
@@ -3,15 +3,95 @@
  * Created by autinerd and margau
  */
 
+// Wifi-Stuff
+
 #include <WiFi.h>
 #include <WiFiAP.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <ArduinoJson.h>
 
+// Display-stuff
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+
+enum v_menu {
+  BOOT,
+  HOME,
+  PROGRAMM,
+  DETAILS
+};
+
+v_menu v_current = BOOT;
+int input = -1;
+
+#define BOOTSCREEN_DUR 2000
+#define EFFECT_DISPLAY_DUR 2000
+
+// Timer
+unsigned long now = 0;
+unsigned long v_effect_display = 0;
+
+// General declarations
+
 #define VERSION "0.0.1-dev"
 
 WebServer server(80);
+
+void initDisplay() {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+  }
+  
+  display.clearDisplay();
+  display.setTextSize(2); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+  display.println(F("ardulumen"));
+  display.setTextSize(1);
+  display.setCursor(40, 25);
+  display.println(VERSION);
+  display.display();      // Show initial text
+}
+
+void displayHome() {
+  display.clearDisplay();
+  display.setTextSize(2); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+  display.println("LIVE MODE");
+  v_current = HOME;
+}
+void displayEffect(String s) {
+  Serial.println("Display Effect "+s);
+  display.setTextSize(2); // Draw 2X-scale text
+  display.fillRect(0, 32, display.width(), 32, SSD1306_WHITE);
+  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); 
+  display.setCursor(10, 36);
+  display.println(s);
+}
+
+void buttonClick(char i) {
+  input = i;
+  Serial.print("Button pressed: ");
+  Serial.println(input, HEX);
+}
+
+void handleInputs() {
+  // First: Serial inputs
+  if (Serial.available() > 0) {
+    char incomingByte = Serial.read();
+    if(incomingByte >= '0' && incomingByte <= '9') {
+      incomingByte -= 48;
+      buttonClick(incomingByte);
+    }
+  }
+}
 
 void handleRoot() {
   server.send(200, "text/plain", "ardulumen-control");
@@ -78,8 +158,41 @@ void setup(void) {
 
   server.begin();
   Serial.println("HTTP server started");
+  // User-Interface
+  
+  initDisplay();
+  Serial.println("User Interface initialized");
 }
 
 void loop(void) {
+  now = millis();
+  // Handle button press stuff
+  handleInputs();
+  // View Switching logic
+  if(v_current==BOOT) {
+    if(now > BOOTSCREEN_DUR) {
+      displayHome();
+    }
+  }
+  if(now > (v_effect_display + EFFECT_DISPLAY_DUR)) {
+    displayHome();
+  }
+  // Home/Live
+  if(input >=0) {
+    displayHome();
+    char effectname [10];
+    switch(input) {
+      case 0:
+      displayEffect(F("BLACKOUT"));
+      break;
+      case 1 ... 9:
+      snprintf(effectname, 10, "EFFECT %d",input);
+      displayEffect(effectname);
+      break;
+    }
+    v_effect_display = now;
+    input = -1;
+  }
+  display.display();
   server.handleClient();
 }
